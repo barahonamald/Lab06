2d18d7d2d8404048be2f845df1150200987d7d6d
Gabriel <barahonamald@wisc.edu>
2023-11-06 15:47:43 -0600

    Initional Commit

--- app/src/main/java/com/cs407/lab6_milestone/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestone/MainActivity.java
@@ -1,20 +1,29 @@
 package com.cs407.lab6_milestone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     // somewhere in Australia
     private final LatLng mDestionationLatLng = new LatLng(-33.8523341, 151.2106085);
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
 
     @Override
@@ -29,6 +38,54 @@ public class MainActivity extends FragmentActivity {
             googleMap.addMarker(new MarkerOptions()
                     .position(mDestionationLatLng)
                     .title("Destination"));
+            displayMyLocation();
         });
+
+        mFusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation() {
+        // Check if permission is granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        // if not, ask for it
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // if permission granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null) {
+                            mMap.addPolyline(new PolylineOptions().add(
+                              new LatLng(mLastKnownLocation.getLatitude(),
+                                      mLastKnownLocation.getLongitude()), mDestionationLatLng));
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Handles the result of the request for location permissions
+     *
+     * @param requestCode The request code passed
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
+     *     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+            // if request is cancelled, the result arrays are empty
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                displayMyLocation();
     }
 }
